5a8654053e4e5d7eb26b816449945b05a4f615e4
Ivan Shpynda <ivanshpynda@gmail.com>
2021-10-14 21:41:22 +0300

    first version

---
+++ app.js
@@ -0,0 +1,673 @@
+const data = [
+  {
+    id: 1,
+    year: '1914 - 1918',
+    place: 'unknown',
+    description: 'Степан Шпинда у військовій формі монархії Габсбургів',
+    src: './img/1.jpg',
+    alt: 'Stepan Shpynda in WW1 uniform',
+  },
+
+  {
+    id: 2,
+    year: '1925',
+    place: 'Збоіська',
+    description: '',
+    src: './img/1925-zboiska.jpg',
+    alt: '1925-zboiska',
+  },
+
+  {
+    id: 3,
+    year: '',
+    place: '',
+    description: 'Бабця (Марія Шипак) та родина',
+    src: './img/babcia.jpg',
+    alt: 'babcia-shypaky',
+  },
+
+  {
+    id: 4,
+    year: '1932',
+    place: 'Перемишль',
+    description: '',
+    src: './img/1932-przemysl.jpg',
+    alt: '1932-przemysl',
+  },
+
+  {
+    id: 5,
+    year: '1932',
+    place: 'Перемишль',
+    description: '',
+    src: './img/1932-przemysl-2.jpg',
+    alt: '1932-przemysl',
+  },
+
+  {
+    id: 6,
+    year: '1933',
+    place: 'Карликів',
+    description: '',
+    src: './img/1933-karlikow.jpg',
+    alt: '1933-krlikow',
+  },
+
+  {
+    id: 7,
+    year: '1933, 6 червня',
+    place: '',
+    description: 'Весілля цьотки Марини',
+    src: './img/1933-wedding.jpg',
+    alt: '1933-wedding',
+  },
+
+  {
+    id: 8,
+    year: '1934, квітень',
+    place: 'Перемишль',
+    description: 'Отець Фаль є на фото',
+    src: './img/1934-przemysl-01.jpg',
+    alt: '1934-przemysl',
+  },
+
+  {
+    id: 9,
+    year: '1934',
+    place: 'Перемишль',
+    description: 'Іван Шпинда з хрестом під час процесії у Перемишлі',
+    src: './img/1934-przemysl-02.jpg',
+    alt: '1934-przemysl',
+  },
+
+  {
+    id: 10,
+    year: '1934',
+    place: 'Пикуличі',
+    description: 'У Пикуличах вже у мієвоєнний період існував український військовий цвинтар',
+    src: './img/1934-pikulice.jpg',
+    alt: '1934-pikulice',
+  },
+
+  {
+    id: 11,
+    year: 'Зима 1934-1935 року',
+    place: 'Перемишль',
+    description: '',
+    src: './img/1934-35-przemysl.jpg',
+    alt: '1934-przemysl',
+  },
+
+  {
+    id: 12,
+    year: '5 березня 1934',
+    place: 'Перемишль',
+    description: '',
+    src: './img/1934-przemysl-03.jpg',
+    alt: '1934-przemysl',
+  },
+
+  {
+    id: 13,
+    year: '1934',
+    place: 'Перемишль',
+    description: '',
+    src: './img/1934-przemysl-03.jpg',
+    alt: '1934-przemysl',
+  },
+
+  {
+    id: 14,
+    year: '1936',
+    place: 'Перемишль',
+    description: '',
+    src: './img/1936-przemysl.jpg',
+    alt: '1936-przemysl',
+  },
+
+  {
+    id: 15,
+    year: '9 листопада 1936',
+    place: '',
+    description: '',
+    src: './img/1936-01.jpg',
+    alt: '1936',
+  },
+
+  {
+    id: 16,
+    year: '1937',
+    place: 'Карликів',
+    description: '',
+    src: './img/1937-karlikow.jpg',
+    alt: '1937',
+  },
+
+  {
+    id: 17,
+    year: '1937 травень',
+    place: '',
+    description: '',
+    src: './img/1937-may.jpg',
+    alt: '1937',
+  },
+
+  {
+    id: 18,
+    year: '1937 грудень',
+    place: 'Перемишль',
+    description: 'Праворуч Стефан Дзюбина',
+    src: './img/1937-december.jpg',
+    alt: '1937',
+  },
+
+  {
+    id: 19,
+    year: '1938',
+    place: 'Перемишль',
+    description: 'Степан (ліворуч) та Іван Шпинди',
+    src: './img/1938-przemysl.jpg',
+    alt: '1938-przemysl',
+  },
+
+  {
+    id: 20,
+    year: 1941,
+    place: 'Криниця',
+    description: 'Вчительські курси у Криниці',
+    src: './img/1941-krynycia.jpg',
+    alt: '1041-krynycia'
+  },
+
+  {
+    id: 21,
+    year: '1943',
+    place: 'Сянок',
+    description: '',
+    src: './img/1943-sanok.jpg',
+    alt: '1943-sanok',
+  },
+
+  {
+    id: 22,
+    year: '1946',
+    place: 'Підволочиськ',
+    description: 'Іван Шпинда з дружиною',
+    src: './img/1946-pidvolochysk.jpg',
+    alt: '1946-pidvolochysk',
+  },
+
+  {
+    id: 23,
+    year: '1951',
+    place: 'Підволочиськ, Заднишівка',
+    description: '',
+    src: './img/1951-zadnishivka.jpg',
+    alt: '1951-Pidvolochysk',
+  },
+
+  {
+    id: 24,
+    year: '1951',
+    place: '',
+    description: '',
+    src: './img/1951.jpg',
+    alt: '1951',
+  },
+
+  {
+    id: 25,
+    year: '1951',
+    place: 'Підволочиськ, Заднишівка',
+    description: '',
+    src: './img/1951-zadnishivka.jpg',
+    alt: '1951-Pidvolochysk',
+  },
+
+  {
+    id: 26,
+    year: '1957',
+    place: 'Білий Бір, Biały Bór',
+    description: '',
+    src: './img/1957-bialy-bor.jpg',
+    alt: 'bialy-bor',
+  },
+
+  {
+    id: 27,
+    year: '1958',
+    place: '',
+    description: 'Михайло',
+    src: './img/1958-mychajlo.jpg',
+    alt: '',
+  },
+
+  {
+    id: 28,
+    year: '1960, 27 жовтня',
+    place: 'Київ',
+    description: '',
+    src: './img/1960-kyiv.jpg',
+    alt: '',
+  },
+
+  {
+    id: 29,
+    year: '1961',
+    place: '',
+    description: 'Михайло і Марія',
+    src: './img/1961-mychajlo-maria.jpg',
+    alt: '',
+  },
+
+  {
+    id: 30,
+    year: '1966',
+    place: '',
+    description: 'Петро і Настя',
+    src: './img/1966-petro-nastia.jpg',
+    alt: '',
+  },
+
+  {
+    id: 31,
+    year: '1977',
+    place: '',
+    description: 'Степан',
+    src: './img/1977-stepan.jpg',
+    alt: '',
+  },
+
+  {
+    id: 32,
+    year: '1978',
+    place: '',
+    description: 'Андрій',
+    src: './img/1978-andrij.jpg',
+    alt: '',
+  },
+
+  {
+    id: 33,
+    year: '1979',
+    place: '',
+    description: 'Богдан',
+    src: './img/1979-bogdan.jpg',
+    alt: '',
+  },
+
+  {
+    id: 34,
+    year: '1987',
+    place: '',
+    description: 'Похорон діда Івана',
+    src: './img/1987-pochoron.jpg',
+    alt: '',
+  },
+
+  {
+    id: 35,
+    year: '1974, 15 лютого',
+    place: '',
+    description: 'Похорон Степана Шпинди',
+    src: './img/1974-pochoron.jpg',
+    alt: '',
+  },
+
+  {
+    id: 36, 
+    year: '1974, 15 II',
+    place: '',
+    description: 'Похорон діда Степана',
+    src: './img/1974-pochoron-02.jpg',
+    alt: '',
+  },
+
+  {
+    id: 37,
+    year: '1928',
+    place: 'Збоіска',
+    description: '',
+    src: './img/1928-zboiska.jpg',
+    alt: '1928-zboiska',
+  },
+
+  {
+    id: 38,
+    year: '1960-ті',
+    place: 'Тернопіль',
+    description: 'Степан, Іван та дружина Менціцького',
+    src: './img/stepan-ivan-1960.jpg',
+    alt: '',
+  },
+
+  {
+    id: 39,
+    year: 'Початок 1970-х',
+    place: 'Тернопіль',
+    description: '',
+    src: './img/ternopil-1970.jpg',
+    alt: '',
+  },
+
+  {
+    id: 40,
+    year: '',
+    place: '',
+    description: '',
+    src: './img/did-i-otec-fal.jpg',
+    alt: '',
+  },
+
+  {
+    id: 41,
+    year: '',
+    place: '',
+    description: '',
+    src: './img/img484.jpg',
+    alt: '',
+  },
+
+  {
+    id: 42,
+    year: '',
+    place: '',
+    description: '',
+    src: './img/img487.jpg',
+    alt: '',
+  },
+
+  {
+    id: 43,
+    year: '',
+    place: '',
+    description: '',
+    src: './img/img488.jpg',
+    alt: '',
+  },
+
+  {
+    id: 44,
+    year: '',
+    place: '',
+    description: '',
+    src: './img/img490.jpg',
+    alt: '',
+  },
+
+  {
+    id: 45,
+    year: '',
+    place: '',
+    description: '',
+    src: './img/img499.jpg',
+    alt: '',
+  },
+
+  {
+    id: 46,
+    year: '',
+    place: '',
+    description: '',
+    src: './img/img500.jpg',
+    alt: '',
+  },
+
+  {
+    id: 47,
+    year: '',
+    place: '',
+    description: '',
+    src: './img/img513.jpg',
+    alt: '',
+  },
+
+  {
+    id: 48,
+    year: '',
+    place: '',
+    description: '',
+    src: './img/img521.jpg',
+    alt: '',
+  },
+
+  {
+    id: 49,
+    year: '',
+    place: '',
+    description: '',
+    src: './img/img528.jpg',
+    alt: '',
+  },
+
+  {
+    id: 50,
+    year: '',
+    place: '',
+    description: '',
+    src: './img/img531.jpg',
+    alt: '',
+  },
+
+  {
+    id: 51,
+    year: '',
+    place: '',
+    description: '',
+    src: './img/img541.jpg',
+    alt: '',
+  },
+
+  {
+    id: 52,
+    year: '',
+    place: '',
+    description: '',
+    src: './img/img542.jpg',
+    alt: '',
+  },
+
+  {
+    id: 53,
+    year: '',
+    place: '',
+    description: '',
+    src: './img/img543.jpg',
+    alt: '',
+  },
+
+  {
+    id: 54,
+    year: '',
+    place: '',
+    description: '',
+    src: './img/img547.jpg',
+    alt: '',
+  },
+
+  {
+    id: 55,
+    year: '',
+    place: '',
+    description: '',
+    src: './img/img549.jpg',
+    alt: '',
+  },
+
+  {
+    id: 56,
+    year: '',
+    place: '',
+    description: '',
+    src: './img/img555.jpg',
+    alt: '',
+  },
+
+  {
+    id: 57,
+    year: '',
+    place: '',
+    description: '',
+    src: './img/img557.jpg',
+    alt: '',
+  },
+
+  {
+    id: 58,
+    year: '',
+    place: '',
+    description: '',
+    src: './img/img569.jpg',
+    alt: '',
+  },
+
+  {
+    id: 59,
+    year: '',
+    place: '',
+    description: '',
+    src: './img/img575.jpg',
+    alt: '',
+  },
+
+  {
+    id: 60,
+    year: '',
+    place: '',
+    description: '',
+    src: './img/img591.jpg',
+    alt: '',
+  },
+
+  {
+    id: 61,
+    year: '',
+    place: '',
+    description: '',
+    src: './img/img595.jpg',
+    alt: '',
+  },
+
+  {
+    id: 62,
+    year: '',
+    place: '',
+    description: '',
+    src: './img/img596.jpg',
+    alt: '',
+  },
+
+  {
+    id: 63,
+    year: '',
+    place: '',
+    description: '',
+    src: './img/img603.jpg',
+    alt: '',
+  },
+
+  {
+    id: 64,
+    year: '',
+    place: '',
+    description: '',
+    src: './img/kursy-krynycia.jpg',
+    alt: '',
+  },
+
+  {
+    id: 65,
+    year: '',
+    place: '',
+    description: '',
+    src: './img/maria-lesia-zemlanski.jpg',
+    alt: '',
+  },
+
+  {
+    id: 66,
+    year: '',
+    place: '',
+    description: '',
+    src: './img/petro-nastia.jpg',
+    alt: '',
+  },
+
+  {
+    id: 67,
+    year: '',
+    place: '',
+    description: '',
+    src: './img/stepan-ivan-1960.jpg',
+    alt: '',
+  },
+
+  {
+    id: 68,
+    year: '',
+    place: '',
+    description: '',
+    src: './img/stepan-paraska.jpg',
+    alt: '',
+  },
+]
+
+
+const container = document.querySelector('.photos');
+
+const renderPhotos = (data) => {
+  let html = '';
+
+  data.forEach(element => {
+    const {id, year, place, description, src, alt} = element;
+
+    const htmlSegment = `<div class="photo">
+    <img
+      src="${src}"
+      alt="${alt}"
+      class="photo__img"
+      onclick="showPhoto('${src}')"
+    >
+    <div class="photo__text-block">
+      <p class="photo__year"><span class="photo__property">ID: </span>${id}</p>
+      <p class="photo__year"><span class="photo__property">Рік: </span>${year}</p>
+      <p class="photo__place"><span class="photo__property">Місце: </span>${place}</p>
+      <p class="photo__description"><span class="photo__property">Опис: </span>${description}</p>
+    </div>
+  </div>`;
+
+  html += htmlSegment;
+  });
+
+  container.innerHTML = html;
+};
+
+renderPhotos(data);
+
+
+const modal = document.getElementById('myModal');
+const modalImage = document.getElementById('img01');
+const caption = document.getElementById('caption');
+const closeModal = document.querySelector('.close');
+
+const showPhoto = (photo) => {
+  modal.style.display = 'block';
+  modalImage.src = photo;
+  modal.classList.add("show-up");
+}
+
+window.addEventListener('click', function(evt) {
+  if (evt.target === modal) {
+    modal.style.display = 'none';
+  }
+});
+
+closeModal.addEventListener('click', function(evt) {
+  modal.style.display = 'none';
+});
+
+window.addEventListener('keydown', function(evt) {
+  if(evt.key === 'Escape') {
+    modal.style.display = 'none';
+  }
+})
